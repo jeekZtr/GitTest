8fdcc7033a957f964a1cbe2d229e34322b69e742
jeekZtr <zhangtairan1116@gmail.com>
2018-09-29 22:52:44 +0800

    say bay dev 1

---
+++ src/main/java/com/news/saybye/Ssybye.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.news.saybye;
+
+/**
+ * @author zhngtr-mi
+ *
+ */
+public class Ssybye {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("say bay dev 1");
+	}
+
+}
