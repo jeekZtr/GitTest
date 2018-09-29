1c67769fe7a8a275c6803336ab488490c25a75c5
jeekZtr <zhangtairan1116@gmail.com>
2018-09-29 21:16:58 +0800

    hello git

--- src/main/java/com/news/GitTest/GitTestClass.java
+++ src/main/java/com/news/GitTest/GitTestClass.java
@@ -3,8 +3,7 @@ package com.news.GitTest;
 public class GitTestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hello git");
 	}
 
 }
